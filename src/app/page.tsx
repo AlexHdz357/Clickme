9b4c906da9741c4ae4e33f455d52fbcf62475ec5
AlexHdz357 <117100191+AlexHdz357@users.noreply.github.com>
2025-02-21 11:44:09 -0600

    Fixed the format and removed extra home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 const Navigation = () => {
+  const [count, setCount] = useState(10);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="min-h-screen bg-white text-black">
       <div className="p-4 flex gap-28 border-b border-black">
@@ -8,11 +19,25 @@ const Navigation = () => {
         <a href="/" className="text-black">Home</a>
         <a href="/page2" className="text-black">Page 2</a>
       </div>
-      <div className="p-4 flex flex-col items-center">
-        <h1 className="text-2xl">0</h1>
-        <button className="mt-4 px-4 py-2 border border-black transition-all duration-300 hover:bg-black hover:text-white">
-          Click me
-        </button>
+      
+      <div className="flex flex-col min-h-[calc(100vh-73px)] items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-800">{count}</h1>
+        
+        <div className="flex flex-col gap-4 items-center">
+          <button 
+            onClick={incrementCount}
+            className="mt-4 px-4 py-2 border border-black transition-all duration-300 hover:bg-black hover:text-white"
+          >
+            Click me
+          </button>
+          
+          <button 
+            onClick={decrementCount}
+            className="px-4 py-2 border border-black transition-all duration-300 hover:bg-black hover:text-white"
+          >
+            Click me
+          </button>
+        </div>
       </div>
     </div>
   );
